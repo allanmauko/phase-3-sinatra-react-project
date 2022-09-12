6183f060b5698f8578db4b4ad4603f8625712dd9
allanmauko <allan.mauko@student.moringaschool.com>
2022-09-12 22:56:33 +0300

    Milestone:deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_09_08_130155) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "model"
     t.integer "daily_rate"
